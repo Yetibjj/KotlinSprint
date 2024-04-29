89d0e2cd4ca3f5d160d71c9e5996c940cc43fcbf
Artlebedev <ar.lebedev@napopravku.ru>
2024-04-30 02:48:18 +0300

    KS-1-3 create information of gagarin flight

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main() {
+    val yearOfFlight: Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+    println(yearOfFlight)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+    print("${hour}:${minute}")
+}
