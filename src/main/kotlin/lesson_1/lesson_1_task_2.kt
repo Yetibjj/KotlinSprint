3fc15a954c8864c8d5404a6090f12107dff9b2b1
Artlebedev <ar.lebedev@napopravku.ru>
2024-04-30 02:33:03 +0300

    KS-1-2 create new variable for Employees

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main() {
+    val countOfOrders: Int = 75
+    val gratitudeText: String = "Благодарим за выбор нашего магазина!"
+    println(countOfOrders)
+    println(gratitudeText)
+
+    var countOfEmployees: Int = 2000
+//    println(countOfEmployees)
+    countOfEmployees = 1999
+    println(countOfEmployees)
+}
