6f84f28c5f0107ca0f8ca2d53c6bce85e45d536d
Artlebedev <ar.lebedev@napopravku.ru>
2024-04-30 20:10:59 +0300

    KS-1-3 fix pr

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -4,11 +4,13 @@ fun main() {
     val yearOfFlight: Int = 1961
     var hour: String = "09"
     var minute: String = "07"
+
     println(yearOfFlight)
     println(hour)
     println(minute)
 
     hour = "10"
     minute = "55"
+
     print("${hour}:${minute}")
 }
