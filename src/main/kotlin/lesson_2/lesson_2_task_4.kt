190ef1fc46a58c30c910988746eb1f14cece3118
Artlebedev <ar.lebedev@napopravku.ru>
2024-05-04 11:09:53 +0300

    KS-3-1 add greetings and minore fix for previous tasks

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,12 +1,12 @@
 package lesson_2
 
-const val ONE_HUNDRED_PERCENT = 100
+const val ONE_HUNDRED_PERCENT_4 = 100
 fun main() {
     val buffPercent = 20
     val crystalOre = 7
     val ironOre = 11
-    val crystalOreWithBuff = (crystalOre * buffPercent) / ONE_HUNDRED_PERCENT
-    val ironOreWithBuff = (ironOre * buffPercent) / ONE_HUNDRED_PERCENT
+    val crystalOreWithBuff = (crystalOre * buffPercent) / ONE_HUNDRED_PERCENT_4
+    val ironOreWithBuff = (ironOre * buffPercent) / ONE_HUNDRED_PERCENT_4
 
     println("Бафф для кристаллической руды: $crystalOreWithBuff")
     println("Бафф для железной руды: $ironOreWithBuff")
--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -1,13 +1,13 @@
 package lesson_2
 
-const val ONE_HUNDRED_PERCENT = 100
+const val ONE_HUNDRED_PERCENT_5 = 100
 fun main() {
     var enteredAmount = 70_000.0
     val percentRate = 16.7
     var countYear = 1
 
     while (countYear <= 20) {
-        val percentOfYear = (enteredAmount * percentRate) / 100
+        val percentOfYear = (enteredAmount * percentRate) / ONE_HUNDRED_PERCENT_5
         enteredAmount += percentOfYear
         countYear++
     }
