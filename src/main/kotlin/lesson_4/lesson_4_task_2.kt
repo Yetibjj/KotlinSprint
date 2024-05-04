47488fbe0332d04e75674d59a5a3029d75b64f87
Artlebedev <ar.lebedev@napopravku.ru>
2024-05-04 22:58:34 +0300

    KS-4-2 add cargo average calc

---
+++ src/main/kotlin/lesson_4/lesson_4_task_2.kt
@@ -0,0 +1,18 @@
+package lesson_4
+
+fun main() {
+    val averageWeightFrom = 35
+    val averageWeightTo = 100
+    val averageVolume = 100
+    val cargo1Weight = 20
+    val cargo1Volume = 80
+    val cargo2Weight = 50
+    val cargo2Volume = 100
+    println(
+        "Груз весом ${cargo1Weight} кг и объемом ${cargo1Volume} л соответствует категории 'Average':" +
+                "${cargo1Weight >= averageWeightFrom && cargo1Weight <= averageWeightTo &&
+                        cargo1Volume < averageVolume}\nГруз весом ${cargo2Weight} кг и объемом ${cargo2Volume}" +
+                "соответствует категории 'Average':${cargo2Weight >= averageWeightFrom && cargo2Weight <= averageWeightTo
+                        && cargo2Volume < averageVolume}"
+    )
+}
