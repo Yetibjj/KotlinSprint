99063ada0655e73d190179dfb8eccf3cfdcad598
Artlebedev <ar.lebedev@napopravku.ru>
2024-05-08 01:11:41 +0300

    KS-5-1 fix captcha

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,10 +1,13 @@
 package lesson_5
 
-const val CAPTCHA_ANSWER = 27
 fun main() {
-    println("Введите ответ на следующее выражение: Два плюс пять умножить на 5")
-    val captcha = readln().toInt()
+    val numberForCaptcha1 = 2
+    val numberForCaptcha2 = 5
+    val expectedResultForCaptcha = numberForCaptcha1 + (numberForCaptcha2 * numberForCaptcha2)
 
-    if (captcha == CAPTCHA_ANSWER) println("Вы авторизовались!")
+    println("Введите ответ на следующее выражение: Два плюс пять умножить на 5")
+    val userAnswer = readln().toInt()
+
+    if (userAnswer == expectedResultForCaptcha) println("Вы авторизовались!")
     else println("Вы бот, доступ запрещён")
 }
